74926ae32bdf53ad14140b630760455b07828db5
Ali Zahir <alizahir@live.com>
2018-11-03 09:16:26 -0400

    pushed

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -21,6 +21,19 @@ class LinksController < ApplicationController
   def edit
   end
 
+  #upvote this
+  def upvote
+    @link = Link.find(params[:id])
+    @link.upvote_by current_user
+    redirect_back fallback_location: root_path
+  end
+
+  def downvote
+    @link = Link.find(params[:id])
+    @link.downvote_by current_user
+    redirect_back fallback_location: root_path
+  end
+
   # POST /links
   # POST /links.json
   def create
